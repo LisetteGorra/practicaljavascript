30f056caab672121b61190bf8a0c1f215556b885
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-01-21 02:34:18 +0000

    Updated code for objects and methods on todo list

--- objects.js
+++ objects.js
@@ -1,15 +1,47 @@
-var myComputer = {
-   operatingSystem: 'mac',     // these lines are called properties followed by a value
-   screenSize: '15 inches', 
-   purchaseYear: 2014,
-};
+function displayTodos() {
+  console.log('My todos:', todos);
+ }
 
-myComputer.operatingSystem; // will return "mac"
+function addTodo(todo) { //addTodo('new Todo') << this value will be pushed to array
+  todos.push(todo);
+  displayTodos(); // this will allow you to see change imediately 
+ }
+ function changeTodo(position, newValue){
+  todos[position] = newValue;
+  displayTodos();
+ }
+  function deleteTodo(position) {
+   todos.splice(position, 1);
+   displayTodos();
+ }
+ 
 
-var lisette = {  // this code will return the name only: 'Lisette'
-	name: 'Lisette',
-	sayName: function() {
-	console.log(this.name);
-  }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var myComputer = {
+//   operatingSystem: 'mac',     // these lines are called properties followed by a value
+//   screenSize: '15 inches', 
+//   purchaseYear: 2014,
+// };
+
+// myComputer.operatingSystem; // will return "mac"
+
+// var lisette = {  // this code will return the name only: 'Lisette'
+// 	name: 'Lisette',
+// 	sayName: function() {
+// 	console.log(this.name);
+//   }
+// }
 
